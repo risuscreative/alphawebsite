7cb0f260572c2a88ef24d23109aebb7c7503fc7c
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-04-04 20:27:30 +0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -44,7 +44,7 @@ export default function Header({ variant }) {
                   >
                     <li className="menu-item-has-children">
                       <NavLink to="/" onClick={() => setMobileToggle(false)}>
-                        Home
+                        <i class="fa-regular fa-house"></i> Home
                       </NavLink>
                       <DropDown>
                         <ul>
