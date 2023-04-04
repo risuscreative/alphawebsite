67265935e871d195fc465165a14f3726de4f1dea
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-04-04 23:37:09 +0300

    Update ContactPage.jsx

--- src/components/Pages/ContactPage.jsx
+++ src/components/Pages/ContactPage.jsx
@@ -34,27 +34,22 @@ export default function ContactPage() {
           <Div className="col-lg-6">
             <form action="#" className="row">
               <Div className="col-sm-6">
-                <label className="cs-primary_color">Full Name*</label>
+                <label className="cs-primary_color">İsim*</label>
                 <input type="text" className="cs-form_field" />
                 <Spacing lg="20" md="20" />
               </Div>
               <Div className="col-sm-6">
-                <label className="cs-primary_color">Email*</label>
+                <label className="cs-primary_color">E-Posta*</label>
                 <input type="text" className="cs-form_field" />
                 <Spacing lg="20" md="20" />
               </Div>
               <Div className="col-sm-6">
-                <label className="cs-primary_color">Project Type*</label>
-                <input type="text" className="cs-form_field" />
-                <Spacing lg="20" md="20" />
-              </Div>
-              <Div className="col-sm-6">
-                <label className="cs-primary_color">Mobile*</label>
+                <label className="cs-primary_color">Soru Başlığı*</label>
                 <input type="text" className="cs-form_field" />
                 <Spacing lg="20" md="20" />
               </Div>
               <Div className="col-sm-12">
-                <label className="cs-primary_color">Mobile*</label>
+                <label className="cs-primary_color">Mesajınız*</label>
                 <textarea
                   cols="30"
                   rows="7"
@@ -72,15 +67,6 @@ export default function ContactPage() {
           </Div>
         </Div>
       </Div>
-      <Spacing lg="150" md="80" />
-      <Div className="cs-google_map">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96652.27317354927!2d-74.33557928194516!3d40.79756494697628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3a82f1352d0dd%3A0x81d4f72c4435aab5!2sTroy+Meadows+Wetlands!5e0!3m2!1sen!2sbd!4v1563075599994!5m2!1sen!2sbd"
-          allowFullScreen
-          title="Google Map"
-        />
-      </Div>
-      <Spacing lg="50" md="40" />
     </>
   );
 }
