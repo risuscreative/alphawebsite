07a24ef82b757a5ec59bc2491788a9d77bb3414a
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-05-02 23:30:06 +0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -44,7 +44,7 @@ export default function Header({ variant }) {
                   >
                     <li>
                       <NavLink to="/" onClick={() => setMobileToggle(false)}>
-                        <i class="fa-solid fa-house fa-lg"></i> Ana Sayfa
+                        <i class="fa-solid fa-house fa-xs"></i> ANA SAYFA
                       </NavLink>
                     </li>
                     <li>
