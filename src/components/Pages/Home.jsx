600fbdaab4383c6aefc450d59ceaabdb0c62b54d
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-06-13 12:11:56 +0300

    Update Home.jsx

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Card from '../Card';
 import FunFact from '../FunFact';
-import Hero from '../Hero';
+import Hero3 from '../Hero/Hero3';
 import Div from '../Div';
 import SectionHeading from '../SectionHeading';
 import Spacing from '../Spacing';
