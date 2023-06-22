e1b78921f8f90f5909129f3c1ba2db4065a34dc0
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-06-22 23:47:46 +0300

    safsafa

--- src/components/Pages/ServicesPage.jsx
+++ src/components/Pages/ServicesPage.jsx
@@ -101,7 +101,7 @@ export default function ServicesPage() {
           </Div>
         </Div>
       </Div>
-      <Spacing lg='150' md='80'/>
+      <Spacing lg='80' md='80'/>
       <Div className="container">
         <SectionHeading
           title='Providing best <br/>pricing for client' 
