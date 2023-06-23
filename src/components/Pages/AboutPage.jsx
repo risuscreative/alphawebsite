ac6a19f96921a319b403f7e5e3546ec9c0ade600
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-06-23 19:06:26 +0300

    Update AboutPage.jsx

--- src/components/Pages/AboutPage.jsx
+++ src/components/Pages/AboutPage.jsx
@@ -41,18 +41,6 @@ export default function AboutPage() {
       />
       {/* End Page Heading Section */}
 
-
-
-      {/* Start Fun Fact Section */}
-      <Div className="container">
-        <FunFact
-          title="Our fun fact"
-          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
-          data={funfaceData}
-        />
-      </Div>
-      {/* End Fun Fact Section */}
-
       {/* Start Why Choose Section */}
       <Spacing lg="100" md="80" />
       <Div className="container">
@@ -97,6 +85,18 @@ export default function AboutPage() {
         </Div>
       </Div>
       {/* End Why Choose Section */}
+
+
+      {/* Start Fun Fact Section */}
+      <Div className="container">
+        <FunFact
+          title="Our fun fact"
+          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
+          data={funfaceData}
+        />
+      </Div>
+      {/* End Fun Fact Section */}
+
 
       {/* Start CTA Section */}
       <Spacing lg="150" md="80" />
