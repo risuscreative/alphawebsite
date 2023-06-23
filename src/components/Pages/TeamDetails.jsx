44c8604d505ffd345b4122dc362228f419fa2803
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-06-23 19:35:15 +0300

    abot

--- src/components/Pages/TeamDetails.jsx
+++ src/components/Pages/TeamDetails.jsx
@@ -29,15 +29,15 @@ export default function TeamDetails() {
           <Div className="col-lg-6 offset-xl-1">
             <Spacing lg='0' md='45'/>
             <Div className="cs-section_heading cs-style1">
-              <h2 className="cs-section_title">Melon Bulgery</h2>
+              <h2 className="cs-section_title">Alpha Graphic Art</h2>
               <Div className="cs-height_10 cs-height_lg_10" />
-              <h3 className="cs-section_subtitle">Product Designer</h3>
+              <h3 className="cs-section_subtitle">Grafik Tasarım Platformu</h3>
               <Div className="cs-height_5 cs-height_lg_5" />
               <Div className="cs-separator cs-accent_bg" />
               <Div className="cs-height_45 cs-height_lg_25" />
-              <p className="cs-m0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium voltire doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+              <p className="cs-m0">2021'den beri gelişmekte olan tecrübem ile sizlere hizmet veriyorum. Bugüne kadar +250 platform ile çalıştım. Uygun fiyata kaliteli tasarımlar yaparak platformunuzu yeniden tasarlıyor, değer katıyorum. Freelance olan iş modelimde ödeme işlemlerini güvenli ödeme linkleri ile veya para transferi yoluyla almaktayım. Yüksek müşteri memnuniyeti olan Alpha'dan özgün ve profesyonel tasarımlar elde edebilirsiniz. </p>
               <Div className="cs-height_25 cs-height_lg_20" />
-              <p className="cs-m0">Ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit.</p>
+              <p className="cs-m0">Yüksek program bilgim ile sizlere 3 programdaki çalışmaları sağlamaktayım. Photoshop, Illustrator ve After Effects kullanarak projelerinizi yeniden tasarlıyor, hareket ve canlılık katıyorum. Örnek çalışmalara ulaşmak veya sipariş vermek için aşağıdaki sosyal medya adreslerimizi ziyaret edebilirsiniz.</p>
               <Div className="cs-height_45 cs-height_lg_30" />
               <SocialWidget/>
             </Div>
