c86eb1254f66c219d8ee9c3072c9a6fb32885b70
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-06-26 16:02:08 +0300

    sa

--- src/components/Widget/ContactInfoWidget.jsx
+++ src/components/Widget/ContactInfoWidget.jsx
@@ -7,7 +7,7 @@ export default function ContactInfoWidget({withIcon, title}) {
       {title && <h2 className="cs-widget_title">{title}</h2>}
       <ul className="cs-menu_widget cs-style1 cs-mp0">
         <li>
-          {withIcon?<span className='cs-accent_color'><Icon icon="material-symbols:add-call-rounded" /></span>:''}
+          {withIcon?<span className='cs-accent_color'><Icon icon="fa-brands fa-discord" /></span>:''}
           +44 454 7800 112
         </li>
         <li>
