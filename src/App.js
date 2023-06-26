02b16eb9a3b5a1a9d68f8c9cf08cdeec232124bd
Just Nyde <88275041+justnyde@users.noreply.github.com>
2023-06-26 12:42:56 +0300

    asdsa

--- src/App.js
+++ src/App.js
@@ -33,10 +33,10 @@ function App() {
   }, [])
 
   if (isLoading == true) return (<>
-    <div style={{height: "100vh", width: "100vh", backgroundColor: "BLACK", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", gap: "2rem", position: "relative"}}>
+    <div style={{height: "100%", width: "100%", backgroundColor: "BLACK", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", gap: "2rem", position: "relative"}}>
       <div style={{height: "5rem", width: "5rem", opacity: "0.4", backgroundColor: "RED", filter: "blur(64px)", position: "absolute", top: "0", left: "0"}}></div>
       <div style={{height: "5rem", width: "5rem", opacity: "0.4", backgroundColor: "RED", filter: "blur(64px)", position: "absolute", bottom: "0", right: "0"}}></div>
-      <svg className="spinner" stroke="WHITE" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="7rem" width="7rem" xmlns="http://www.w3.org/2000/svg"><path opacity="0.2" fillRule="evenodd" clipRule="evenodd" d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="currentColor"></path><path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="currentColor"></path></svg>
+      <svg className="spinner" stroke="WHITE" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="5rem" width="5rem" xmlns="http://www.w3.org/2000/svg"><path opacity="0.2" fillRule="evenodd" clipRule="evenodd" d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="currentColor"></path><path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="currentColor"></path></svg>
     </div>
   </>)
 
