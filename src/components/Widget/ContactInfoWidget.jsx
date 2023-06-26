28ace36da3f638e29689767c75a28ca492aa51f4
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-06-26 16:17:24 +0300

    s

--- src/components/Widget/ContactInfoWidget.jsx
+++ src/components/Widget/ContactInfoWidget.jsx
@@ -8,19 +8,19 @@ export default function ContactInfoWidget({withIcon, title}) {
       <ul className="cs-menu_widget cs-style1 cs-mp0">
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:discord" /></span>:''}
-          +44 454 7800 112
+          discord.gg/7Q7EbNKHha
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:envelope" /></span>:''}
-          infotech@arino.com
+          destek.alphagrafik@gmail.com
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:instagram" /></span>:''}
-          50 Wall Street Suite, 44150 <br/>Ohio, United States
+          @alphagraphicart
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:behance" /></span>:''}
-          50 Wall Street Suite, 44150 <br/>Ohio, United States
+          behance.net/alphagrafik
         </li>
       </ul>
       
