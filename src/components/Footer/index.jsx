6148d1bfda6d6e4eaeb5292c350d86f5129cfc4d
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-07-04 22:58:05 +0300

    s

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,7 +14,7 @@ export default function Footer({copyrightText, logoSrc, logoAlt, text}) {
       href: '/sss'
     },
     {
-      title: 'Ödeme Yöntemleri',
+      title: 'Sipariş Süreci',
       href: '/sss'
     }
   ]
@@ -79,7 +79,7 @@ export default function Footer({copyrightText, logoSrc, logoAlt, text}) {
               </Div>
             <Div className="col-lg-3 col-sm-6">
               <Div className="cs-footer_item">
-                <MenuWidget menuItems={servMenu} menuHeading='Hizmetsler'/>
+                <MenuWidget menuItems={servMenu} menuHeading='Hizmetler'/>
               </Div>
             </Div>
             <Div className="col-lg-3 col-sm-6">
@@ -93,7 +93,7 @@ export default function Footer({copyrightText, logoSrc, logoAlt, text}) {
       <Div className="container">
         <Div className="cs-bottom_footer">
           <Div className="cs-bottom_footer_left">
-            <Div className="cs-copyright">Copyright © 2023 Alpha Graphic Art.</Div>
+            <Div className="cs-copyright">Copyright © 2023 Alpha Graphic Art. Tüm hakları saklıdır.</Div>
           </Div>
           <Div className="cs-bottom_footer_right">
             <MenuWidget menuItems={copyrightLinks} variant=' cs-style2'/>
