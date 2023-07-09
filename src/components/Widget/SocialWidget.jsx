9b98fc8be633a09ee03df91e27048b1d1b1762ff
risuscreative <116668810+risuscreative@users.noreply.github.com>
2023-07-09 16:53:24 +0300

    sa

--- src/components/Widget/SocialWidget.jsx
+++ src/components/Widget/SocialWidget.jsx
@@ -6,13 +6,13 @@ import Div from '../Div';
 export default function SocialWidget() {
   return (
     <Div className="cs-social_btns cs-style1">
-      <a href='https://www.instagram.com/alphagraphicart/' className="cs-center" target="_blank" >
-        <Icon icon="mdi:discord" />
+      <a href='https://discord.gg/7Q7EbNKHha' className="cs-center" target="_blank" >
+        <Icon icon="fa6-brands:discord" />
         </a>
-      <a href='https://www.instagram.com/alphagraphicart/' className="cs-center" target="_blank" >
+      <a href='https://www.behance.net/alphagrafik' className="cs-center" target="_blank" >
         <Icon icon="fa6-brands:behance" />               
         </a>
-      <a href='https://www.instagram.com/alphagraphicart/' className="cs-center" target="_blank" >
+      <a href='https://www.youtube.com/@alphatasarim' className="cs-center" target="_blank" >
         <Icon icon="fa6-brands:youtube" />              
         </a>
       <a href='https://www.instagram.com/alphagraphicart/' className="cs-center" target="_blank" >
